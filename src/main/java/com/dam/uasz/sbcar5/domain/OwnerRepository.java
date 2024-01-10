a28c61d2de64236472320e9c32086cc009e32233
ElBachir Toure <bassirou.toure@esp.sn>
2024-01-10 17:27:05 +0000

    carbackend without authentication

---
+++ src/main/java/com/dam/uasz/sbcar5/domain/OwnerRepository.java
@@ -0,0 +1,5 @@
+package com.dam.uasz.sbcar5.domain;
+
+import  org.springframework.data.repository.CrudRepository;
+
+public interface OwnerRepository extends CrudRepository<Owner, Long> {}
---
+++ src/test/java/com/dam/uasz/sbcar5/SbCar5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dam.uasz.sbcar5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbCar5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
